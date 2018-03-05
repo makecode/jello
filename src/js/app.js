569f6a0c95310e8cca0dad43f263d1bde86bd2ac
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2018-03-05 21:24:47 +0200

    add new inners

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,24 @@
 //=include libs/jquery.min.js
 //=include libs/rateit.js
-'use strict';
 
 $( document ).ready(() => {
+  const audioWrap = $('.ringtone__icon');
 
-	// // your code will be here!
-	// const foo = 'Hello';
-	// const bar = $('.title');
-	// console.log(`${foo}, i'm ES6 template!`, bar);
+  for (let i = 0; i < audioWrap.length; i += 1) {
+    const el = audioWrap[i];
+    $(el).click(function (event) {
+      event.stopPropagation();
+      const audio = $(this).find('.ringtone__audio')[0];
 
+      if (audio.paused) {
+        audio.currentTime = 0;
+        audio.play();
+        $(el).addClass('is-played');
+
+      } else {
+        audio.pause();
+        $(el).removeClass('is-played');
+      }
+    });
+  }
 });
